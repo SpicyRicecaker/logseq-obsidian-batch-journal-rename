d8814fc2e9887930092d3c28375f48e25eb6b3a8
Andy Li <54951411+SpicyRicecaker@users.noreply.github.com>
2022-02-25 00:33:50 -0800

    i hate not avoiding hasty abstractions

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+use anyhow::{Context, Result};
 use std::{env::args, fs, path::PathBuf};
 
 use chrono::NaiveDate;
 use regex::Regex;
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+use log::*;
+fn main() -> Result<()> {
+    env_logger::init();
     // first take the argument of the directory which we must read
     let mut args = args();
     args.next();
 
-    let logseq = Regex::new(r"(?P<content>\d{2}_\d{2}_\d{2})(?P<extension>[^\\/]*\.md)$")?;
+    let logseq = Regex::new(r"(?P<content>\d{4}_\d{2}_\d{2})(?P<extension>[^\\/]*\.md)$")?;
     let obsidian = Regex::new(r"(?P<content>\d{2}\d{2}\d{2})(?P<extension>[^\\/]*\.md)$")?;
     let scuffed = Regex::new(r"(?P<content>\d{2}.\d{2}.\d{2})(?P<extension>[^\\/]*\.md)$")?;
 
@@ -18,59 +22,80 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // create fake dir instead, right next to copied dir
         let old_path: PathBuf = copy_path.into();
 
-        let mut new: PathBuf = old_path.clone();
-
-        new.set_file_name(format!(
+        let new_path: PathBuf = old_path.with_file_name(format!(
             "{}_copy",
-            new.file_name().unwrap().to_str().unwrap()
+            old_path.file_name().unwrap().to_str().unwrap()
         ));
+        trace!("new path is {:?} to {:?}", &old_path, &new_path);
 
-        // remove this dir if it already exists
-        if old_path.is_dir() {
-            fs::remove_dir_all(&old_path)?;
-            // create new dir
-            fs::create_dir(&old_path)?;
-
-            // copy items (non-recursive)
-            for entry in fs::read_dir(old_path)? {
-                let path = entry?.path();
-                let mut t = new.clone();
-                t.push(path.file_name().unwrap());
-                fs::copy(path, t)?;
-            }
-            let folder_to_rename = new;
-
-            // read dir
-            for entry in fs::read_dir(folder_to_rename)? {
-                let path = entry?.path();
-
-                let file_name = path.file_stem().unwrap().to_str().unwrap();
-
-                // we could do loop but we have to then do ew regex stuff
-                let (date, extension) = if let Some(captures) = logseq.clone().captures(file_name) {
-                    (
-                        NaiveDate::parse_from_str(&captures["content"], "%Y_%m_%d")?,
-                        captures["extension"].to_string(),
-                    )
-                } else if let Some(captures) = obsidian.clone().captures(file_name) {
-                    (
-                        NaiveDate::parse_from_str(&captures["content"], "%m%d%y")?,
-                        captures["extension"].to_string(),
-                    )
-                } else if let Some(captures) = scuffed.clone().captures(file_name) {
-                    (
-                        NaiveDate::parse_from_str(&captures["content"], "%m.%d.%y")?,
-                        captures["extension"].to_string(),
-                    )
-                } else {
-                    panic!("pattern not supported");
-                };
-
-                let new_file_name = format!("{}{}", date.format("%m.%d.%y"), extension);
-
-                fs::rename(&path, path.with_file_name(new_file_name))?;
-            }
+        if new_path.is_dir() {
+            // actually create new dir
+            fs::remove_dir_all(&new_path)?;
         }
+        fs::create_dir_all(&new_path)?;
+        trace!("created dir at {:?}", &new_path);
+
+        if !old_path.is_dir() {
+            fs::create_dir_all(&old_path)
+                .with_context(|| format!("error creating dir {:?}", &old_path))?;
+        }
+
+        // copy items (non-recursive)
+        for entry in fs::read_dir(&old_path)
+            .with_context(|| format!("error reading old path, {:?}", old_path))?
+        {
+            let path = entry?.path();
+            let mut t = new_path.clone();
+            t.push(path.file_name().unwrap());
+            fs::copy(&path, &t)
+                .with_context(|| format!("error copying from {:?} to {:?}", path, t))?;
+        }
+
+        // read dir (in new copied dir)
+        for entry in fs::read_dir(&new_path)
+            .with_context(|| format!("error reading old path, {:?}", old_path))?
+        {
+            let path = entry?.path();
+
+            let file_name = path.file_name().unwrap().to_str().unwrap();
+
+            // we could do loop but we have to then do ew regex stuff
+            let (date, extension) = if let Some(captures) = logseq.clone().captures(file_name) {
+                (
+                    NaiveDate::parse_from_str(&captures["content"], "%Y_%m_%d").with_context(
+                        || format!("error converting {} for logseq", &captures["content"]),
+                    )?,
+                    captures["extension"].to_string(),
+                )
+            } else if let Some(captures) = obsidian.clone().captures(file_name) {
+                (
+                    NaiveDate::parse_from_str(&captures["content"], "%m%d%y").with_context(
+                        || format!("error converting {} for obsidian", &captures["content"]),
+                    )?,
+                    captures["extension"].to_string(),
+                )
+            } else if let Some(captures) = scuffed.clone().captures(file_name) {
+                (
+                    NaiveDate::parse_from_str(&captures["content"], "%m.%d.%y").with_context(
+                        || format!("error converting {} for scuffed", &captures["content"]),
+                    )?,
+                    captures["extension"].to_string(),
+                )
+            } else {
+                error!("the pattern {} is not supported", file_name);
+                panic!();
+            };
+
+            let new_file_name = format!("{}{}", date.format("%m.%d.%y"), extension);
+
+            let mut new_path = new_path.clone();
+            new_path.push(new_file_name);
+
+            fs::rename(&path, &new_path)?;
+            trace!("renamed {:?} to {:?}", &path, &new_path);
+        }
+    } else {
+        error!("no args provided. Usage: [rename] [path_to_folder]");
     }
     Ok(())
 }
